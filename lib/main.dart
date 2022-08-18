5a648ebd5299f5895c5d7e1c58c70eae2a44d12f
Mateo Gonzalez Santos <mateosantos969@gmail.com>
2022-08-18 17:47:03 -0500

    Fin seccion 2

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bad_names_app/pages/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => HomeScreen()
+      },
+
+    );
+  }
+}
